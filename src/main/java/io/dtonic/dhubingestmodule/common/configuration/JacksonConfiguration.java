4851ce99b242dfd69141f3819b377bb9869e5fa2
kyounghwan kim <dolkys123@sju.ac.kr>
2022-09-23 13:45:52 +0900

    Feat: Add Custom Object Mapper
    NiFi에서 가져온 데이터를 Jackson Object Mapper를 통해 객체로 변환해주기 위해
    MultiDateDeserializer Module을 생성하여, 여러 Date형식에 호환되는 Object Mapper를 구현.

---
+++ src/main/java/io/dtonic/dhubingestmodule/common/configuration/JacksonConfiguration.java
@@ -0,0 +1,38 @@
+package io.dtonic.dhubingestmodule.common.configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import io.dtonic.dhubingestmodule.common.code.Constants;
+import io.dtonic.dhubingestmodule.common.serialize.MultiDateDeserializer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Date.class, new MultiDateDeserializer());
+        objectMapper.registerModule(module);
+        objectMapper.setSerializationInclusion(Include.NON_NULL);
+        // objectMapper.setDateFormat(new SimpleDateFormat(Constants.NIFI_CONTENT_DATE_FORMAT));
+        // objectMapper.setTimeZone(TimeZone.getDefault());
+        return objectMapper;
+    }
+
+    // 결국에는 multiDateDeseializer는 안씀
+    @Bean
+    public ObjectMapper nifiObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setTimeZone(TimeZone.getDefault());
+        objectMapper.setDateFormat(new SimpleDateFormat(Constants.NIFI_CONTENT_DATE_FORMAT));
+        objectMapper.setSerializationInclusion(Include.NON_NULL);
+        return objectMapper;
+    }
+}
