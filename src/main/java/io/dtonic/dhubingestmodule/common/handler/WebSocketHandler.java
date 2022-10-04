83f63375be20116084c289a7524825fabd87a18a
kyounghwan kim <dolkys123@sju.ac.kr>
2022-10-04 14:03:40 +0900

    Fix : websocket scheduling fix

--- src/main/java/io/dtonic/dhubingestmodule/common/handler/WebSocketHandler.java
+++ src/main/java/io/dtonic/dhubingestmodule/common/handler/WebSocketHandler.java
@@ -1,15 +1,11 @@
 package io.dtonic.dhubingestmodule.common.handler;
 
-import ch.qos.logback.core.net.server.Client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dtonic.dhubingestmodule.pipeline.service.PipelineSVC;
 import io.dtonic.dhubingestmodule.pipeline.vo.PipelineListResponseVO;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import javax.websocket.OnMessage;
-import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,7 +15,6 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-@Slf4j
 @Component
 @EnableScheduling
 public class WebSocketHandler extends TextWebSocketHandler {
@@ -42,12 +37,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
         CLIENTS.remove(session.getId());
     }
 
-    //@Scheduled(fixedDelay = 2000, initialDelay = 1000)
+    @Scheduled(fixedDelay = 2000, initialDelay = 1000)
     public void updatePipelineStatus() throws Exception {
-        String searchObject = null;
-        String searchValue = null;
-        String status = null;
-
         List<PipelineListResponseVO> result = pipelineSVC.getPipelineList();
 
         CLIENTS
