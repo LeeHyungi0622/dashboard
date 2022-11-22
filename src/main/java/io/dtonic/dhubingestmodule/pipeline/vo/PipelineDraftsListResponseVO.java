8ca5f34a96f0ef0c9e2fbf1ba047484dc2647911
kyounghwan kim <dolkys123@sju.ac.kr>
2022-11-22 14:13:11 +0900

    Fix : VO add timezone

--- src/main/java/io/dtonic/dhubingestmodule/pipeline/vo/PipelineDraftsListResponseVO.java
+++ src/main/java/io/dtonic/dhubingestmodule/pipeline/vo/PipelineDraftsListResponseVO.java
@@ -1,6 +1,9 @@
 package io.dtonic.dhubingestmodule.pipeline.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.dtonic.dhubingestmodule.common.code.Constants;
+
 import java.util.Date;
 import lombok.Data;
 
@@ -10,7 +13,7 @@ public class PipelineDraftsListResponseVO {
     private Integer id;
     private String name;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss z")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss z", timezone = Constants.CONTENT_DATE_TIMEZONE)
     private Date modifiedAt;
 
     private Boolean isCollector;
--- src/main/java/io/dtonic/dhubingestmodule/pipeline/vo/PipelineListResponseVO.java
+++ src/main/java/io/dtonic/dhubingestmodule/pipeline/vo/PipelineListResponseVO.java
@@ -3,6 +3,9 @@ package io.dtonic.dhubingestmodule.pipeline.vo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+
+import io.dtonic.dhubingestmodule.common.code.Constants;
+
 import java.util.Date;
 import lombok.Data;
 
@@ -16,9 +19,9 @@ public class PipelineListResponseVO {
     private String dataSet;
     private String status;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss z")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss z", timezone = Constants.CONTENT_DATE_TIMEZONE)
     private Date createdAt;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss z")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss z", timezone = Constants.CONTENT_DATE_TIMEZONE)
     private Date modifiedAt;
 }
